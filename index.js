761237ea33d1dd7827f94adfdc2c01dbf7ffef69
basant-yadav-5ex <145121816+basant-yadav-5ex@users.noreply.github.com>
2025-12-19 14:59:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ quiet: true });
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.send("Hello Fortknox user");
+});
+
+const serverPort = process.env.PORT || 3000;
+
+app.listen(serverPort, () => {
+  console.log(`Server running on port: ${serverPort}`);
+});
